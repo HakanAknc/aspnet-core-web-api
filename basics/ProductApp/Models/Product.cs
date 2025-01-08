8e3e98e3a113ec92434966df6b648f236c775d1f
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2025-01-08 10:01:34 +0300

    Add files via upload

---
+++ basics/ProductApp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String? ProductName { get; set; }   // string? demek "null" bırakılabilir demek
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
