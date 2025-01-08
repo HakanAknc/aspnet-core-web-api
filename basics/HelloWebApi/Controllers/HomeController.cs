9f09c7aa9b47f730462ec99c9e36e69fd1e27f76
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2025-01-08 10:00:37 +0300

    Add files via upload

---
+++ basics/HelloWebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using HelloWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HelloWebApi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ResponseModel GetMessage()
+        {
+            return new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello ASP.NET Core Web API"
+            };
+        }
+    }
+}
