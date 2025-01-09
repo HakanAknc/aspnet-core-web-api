2aacedef7836f8b8a06271d553ad8043bbf6c311
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2025-01-09 09:34:49 +0300

    Add files via upload

---
+++ basics/foodDemo/Data/ApplicationContext.cs
@@ -0,0 +1,9 @@
+﻿using foodDemo.Models;
+
+namespace foodDemo.Data
+{
+    public class ApplicationContext
+    {
+        public static List<FoodItem> MenuItems { get; set; } = new List<FoodItem>();
+    }
+}
