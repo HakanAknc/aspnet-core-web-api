05390e3fc0d46ddc8b5d395c526a314f468dae96
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2025-01-21 17:19:53 +0300

    Add files via upload

---
+++ basics/ProductWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebApi.Models;
+
+namespace ProductWebApi.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetProducts()
+        {
+            var product = new List<Product>()
+            {
+                new Product() {Id=1, ProductName="Computer"},
+                new Product() {Id=2, ProductName="Keyboard"},
+                new Product() {Id=3, ProductName="Mouse"}
+            };
+            _logger.LogInformation("GetAllProducts action has been called.");
+            return Ok(product);
+
+        }
+    }
+}
---
+++ basics/ProductWebApi/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductWebApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String? ProductName { get; set; }  // Geçerli bir String veya null olabilir
+
+    }
+}
